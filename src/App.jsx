800a1d7c44c5127801d94725954185386fb22b27
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-04-04 21:58:20 +0200

    Final result

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+
+import './style/style.scss'
+
+function App() {
+
+  return (
+    <>
+
+      <article>
+        <div className='div_img_h1_p'>
+          <img src="https://images.pexels.com/photos/1389429/pexels-photo-1389429.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt='photo album' />
+          <div className='div_h1_p'>
+            <h1>Rozes</h1>
+            <p>Under the Grave</p>
+            <p>(2016)</p>
+          </div>
+        </div>
+
+        <div className='bandeau_bas'>
+          <h3>Rate this album</h3>
+          <h2>☆☆☆☆☆</h2>
+
+        </div>
+
+
+      </article>
+
+
+    </>
+  )
+}
+
+export default App
